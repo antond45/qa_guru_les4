be482683c2c2a790ef8b76ba339af1cc9cc1aa01
Darin Anton <adarin@t1-consulting.ru>
2024-04-01 21:38:35 +0300

    deleted extra lines

--- src/test/java/tests/TestHoverGitHub.java
+++ src/test/java/tests/TestHoverGitHub.java
@@ -16,9 +16,7 @@ public class TestHoverGitHub {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000;
     }
 
     //Task1
